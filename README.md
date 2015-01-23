ee54c646d064d09afc95d98b9b103cc92fbc7c96
Wout Mertens <Wout.Mertens@gmail.com>
2015-01-23 10:19:52 +0100

    Allow opening a new window too

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 global-chrome-tab
 =================
 
-Chrome extension for opening a new tab with a global shortcut
+Chrome extension for opening a new tab or window with a global shortcut.
+
+This is very handy, as evidenced by the fact that Chromebooks have a dedicated Search button.
+
+To use, install the extension and in the keyboard shortcuts section at the bottom, set a shortcut for the new tab or new window action and make it Global.
--- background.js
+++ background.js
@@ -3,9 +3,14 @@ chrome.commands.onCommand.addListener(function(command) {
     // First we need to focus the current window
     // otherwise the new tab won't have the cursor in the OmniBar
     chrome.windows.getCurrent({}, function(win){
-     chrome.windows.update(win.id, {focused: true}, function() {
-      chrome.tabs.create({});
+      chrome.windows.update(win.id, {focused: true}, function() {
+        chrome.tabs.create({});
+      });
     });
-   });
+  } else if (command === 'new-window') {
+    // Simply passing focused doesn't seem to work so try harder
+    chrome.windows.create({focused: true}, function(win){
+      chrome.windows.update(win.id, {focused: true});
+    });
   }
 });
